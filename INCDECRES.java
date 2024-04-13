147ba005999efc5fbe27cd30fa609b44a24a7636
Shubhangi Srivastava <136235561+Shubhangi-srivastav@users.noreply.github.com>
2024-04-13 18:43:43 +0530

    Create INCDECRES.java

---
+++ INCDECRES.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class INCDECRES{
+      public static void main(Strings args[]){
+             int arr[] ={7,4,2,8,9,1,3,6}
+             int n= arr.length;
+             Arrays.sort(arr);
+             for(int i=0;i<n/2;i++){
+                  System.out.print(arr[i] + " ")
+             }for(int i=n-1;i>=n/2;i--){
+                  System.out.print(arr[i] + " ")
+             }
+             }}
